97448d7ee0743a5bb2a5bef0cfd7942a453dafd0
rxd-pro <ramratidahiya8@gmail.com>
2026-04-23 15:38:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post('/user/updateusername', UserController.updateUsername);
 app.get('/user/deleteaccount', UserController.deleteAccount);
 //app.post('/user/linkplatform', UserController.linkPlatform);
 //app.post('/user/unlinkplatform', UserController.unlinkPlatform);
-app.get("/shared/:version/:type", sendShared);
+//app.get("/shared/:version/:type", sendShared);
 //app.post('/user/profile', UserController.getProfile);
 app.post('/user-equipped-cosmetics/update', UserController.updateCosmetics);
 app.post('/user/cosmetics/addskin', UserController.addSkin);
