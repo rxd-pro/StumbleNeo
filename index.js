196ac3328f45895d373a7cc5072387156a207ec0
rxd-pro <ramratidahiya8@gmail.com>
2026-05-20 14:41:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const {
 
 const app = express();
 const Title = "StumbleNeo";
-const PORT = process.env.PORT || 0.0.0.0:8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.get('/version/get', (req, res) => {
